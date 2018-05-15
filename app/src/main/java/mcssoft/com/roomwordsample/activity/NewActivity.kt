a71be270a1100763dd7847fdf144dce9f31c7115
mattcs2401 <03m777c22s>
2018-05-15 20:33:22 +1000

    Code changes.

--- app/src/main/java/mcssoft/com/roomwordsample/activity/NewActivity.kt
+++ app/src/main/java/mcssoft/com/roomwordsample/activity/NewActivity.kt
@@ -1,7 +1,15 @@
 package mcssoft.com.roomwordsample.activity
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import mcssoft.com.roomwordsample.R
 
 class NewActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+        setSupportActionBar(toolbar)
+    }
 }
